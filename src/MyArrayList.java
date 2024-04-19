f39f6e92323b022a4c6954addc98ffc3bf88c95d
Michael Korelov <michaelkorelov@Michaels-MacBook-Air.local>
2024-04-19 13:58:19 -0700

    initial setup

---
+++ src/MyArrayList.java
@@ -0,0 +1,52 @@
+/*
+ * ArrayList implementation in Java based on the pseudocode and 
+ * method descriptions provided at: 
+ * https://cogniterra.org/lesson/36902/step/1?unit=28666 & 
+ * https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html.
+ * Uses array as the backend data structure. Implements insert,
+ * find, get, remove, and size.
+ */
+public class MyArrayList {
+    private static int DEFAULT = 10;
+    private int size;
+    private int[] arr;
+
+    /*
+     * TODO
+     */
+    public MyArrayList() {
+    }
+
+    /*
+     * TODO
+     */
+    public MyArrayList(int n) {
+    }
+
+    /*
+     * TODO
+     */
+    public int get(int idx) {
+        return 0;
+    }
+
+    /*
+     * TODO
+     */
+    public void insert(int elem) {
+    }
+
+    /*
+     * TODO
+     */
+    public boolean find(int elem) {
+        return true;
+    }
+
+    /*
+     * TODO
+     */
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/MyArrayListTester.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MyArrayListTester {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+
+    @Test
+    public void test() {
+        MyArrayList ls = new MyArrayList();
+    }
+}
