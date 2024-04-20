2a7909b2c115d4d776905340615b191abeef50b2
Michael Korelov <michaelkorelov@Michaels-MacBook-Air.local>
2024-04-19 17:34:05 -0700

    sanity tests

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,52 +1,64 @@
 /*
- * ArrayList implementation in Java based on the pseudocode and 
+ * Basic ArrayList implementation in Java based on the pseudocode and 
  * method descriptions provided at: 
  * https://cogniterra.org/lesson/36902/step/1?unit=28666 & 
  * https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html.
- * Uses array as the backend data structure. Implements insert,
- * find, get, remove, and size.
+ * Uses array as the backend data structure. 
+ * 
+ * 1) Write sanity tests for basic methods.
+ * 2) Implement add(idx, elem), contains(elem), remove(idx), & 
+ * getCapacity(). 
+ * 3) Write advanced tests w/ edge cases.
+ * 4) Write sanity tests for advanced methods.
+ * 5) Implement trimToSize(), toArray(), sort(), size(), set(idx, elem), 
+ * remove(elem), isEmpty(), indexOf(elem), get(idx), ensureCapacity(sz), 
+ * clear(), & add(elem). 
+ * 6) Implement the constructor given an array, implement getCapacity(),
+ * and update the access modifiers of member variable to private.
+ * 7) Write advanced tests w/ edge cases.
  */
 public class MyArrayList {
-    private static int DEFAULT = 10;
-    private int size;
-    private int[] arr;
+    private static int DEFAULT = 10;                // default capacity of MyArrayList
+    public int size;                               // number of elements in MyArrayList
+    public int capacity;                           // size of backend array in MyArrayList
+    public Object[] array;                         // backend array in MyArrayList
 
     /*
      * TODO
      */
     public MyArrayList() {
+        this.array = new Object[DEFAULT];
+        this.capacity = DEFAULT;
+        this.size = 0;
     }
 
     /*
      * TODO
      */
-    public MyArrayList(int n) {
+    public MyArrayList(int cap) {
+        this.array = new Object[cap];
+        this.capacity = cap;
+        this.size = 0;
     }
 
     /*
      * TODO
      */
-    public int get(int idx) {
-        return 0;
-    }
-
-    /*
-     * TODO
-     */
-    public void insert(int elem) {
-    }
-
-    /*
-     * TODO
-     */
-    public boolean find(int elem) {
+    public boolean add(Object elem, int idx) {
         return true;
     }
 
     /*
      * TODO
      */
-    public int size() {
-        return 0;
+    public boolean contains(Object elem) {
+        return true;
+    }
+
+    /*
+     * TODO
+     */
+    public Object remove(int idx) {
+        return "";
     }
 }
--- src/MyArrayListTester.java
+++ src/MyArrayListTester.java
@@ -3,11 +3,81 @@ import org.junit.Test;
 
 public class MyArrayListTester {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
     }
 
     @Test
-    public void test() {
+    public void testDefaultConstructor() {
         MyArrayList ls = new MyArrayList();
+        assertEquals(ls.size, 0);
+        assertEquals(ls.capacity, 10);
+    }
+
+    @Test
+    public void testConstructor() {
+        MyArrayList ls = new MyArrayList(20);
+        assertEquals(ls.size, 0);
+        assertEquals(ls.capacity, 20);
+    }
+
+    @Test
+    public void testAdd() {
+        MyArrayList ls = new MyArrayList();
+
+        assertEquals(ls.add("Cat",0), true);
+        assertEquals(ls.array[0], "Cat");
+
+        assertEquals(ls.add("Dog", 1), true);
+        assertEquals(ls.array[1], "Dog");
+
+        assertEquals(ls.add("Snake", 0), true);
+        assertEquals(ls.array[0], "Snake");
+        assertEquals(ls.array[1], "Cat");
+        assertEquals(ls.array[2], "Dog");
+
+        assertEquals(ls.add("Fish", 5), false);
+
+        assertEquals(ls.add("Pig", 100), false);
+
+        assertEquals(ls.add("Hamster", -1), false);
+    }
+
+    @Test
+    public void testContains() {
+        MyArrayList ls = new MyArrayList();
+        ls.add("Cat",0);
+        ls.add("Dog", 1);
+        ls.add("Snake", 0);
+        ls.add("Fish", 5);
+        ls.add("Pig", 100);
+        ls.add("Hamster", -1);
+
+        assertEquals(ls.contains("Cat"), true);
+        assertEquals(ls.contains("Dog"), true);
+        assertEquals(ls.contains("Snake"), true);
+        assertEquals(ls.contains("Fish"), false);
+        assertEquals(ls.contains("Pig"), false);
+        assertEquals(ls.contains("Hamster"), false);
+    }
+
+    @Test
+    public void testRemove() {
+        MyArrayList ls = new MyArrayList();
+        ls.add("Cat",0);
+        ls.add("Dog", 1);
+        ls.add("Snake", 0);
+        ls.add("Fish", 5);
+        ls.add("Pig", 100);
+        ls.add("Hamster", -1);
+
+        assertEquals(ls.remove(5), null);
+        assertEquals(ls.remove(0), "Snake");
+        assertEquals(ls.remove(0), "Cat");
+        assertEquals(ls.remove(2), null);
+        assertEquals(ls.remove(100), null);
+        assertEquals(ls.remove(0), "Dog");
+
+        assertEquals(ls.contains("Snake"), false);
+        assertEquals(ls.contains("Cat"), false);
+        assertEquals(ls.contains("Dog"), false);
     }
 }
