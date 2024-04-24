ab9ed821a0359d6261fe0569c783db3f8247e48b
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-04-23 20:28:15 -0700

    incomplete coding style

--- src/Main.java
+++ src/Main.java
@@ -1,55 +1,24 @@
 ///////////////////////////////////////////////////////////////////////////////
-//                   ALL STUDENTS COMPLETE THESE SECTIONS
-// Title:              (program's title)
-// Files:              (list of source files)
-// Quarter:            (course) Spring 2020
+// Title:              MyArrayList
+// Files:              MyArrayList.java
 //
-// Author:             (your name)
-// Email:              (your email address)
-// Instructor's Name:  (name of your instructor)
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-//                  CHECK ASSIGNMENT PAGE TO see IF PAIR-PROGRAMMING IS ALLOWED
-//                  If pair programming is allowed:
-//                  1. Read PAIR-PROGRAMMING policy
-//                  2. Choose a partner wisely
-//                  3. Complete this section for each program file
-//
-// Pair Partner:        (name of your pair programming partner)
-// Email:               (email address of your programming partner)
-// Instructors's Name:  (name of your partner's instructor)
-// Lab Section:         (your partner's lab section number)
-//
-//////////////////// STUDENTS WHO GET HELP FROM OTHER THAN THEIR PARTNER //////
-//                   must fully acknowledge and credit those sources of help.
-//                   Instructors and TAs do not have to be credited here,
-//                   but roommates, relatives, strangers, etc do.
-//
-// Persons:          Identify persons by name, relationship to you, and email.
-//                   Describe in detail the the ideas and help they provided.
-//
-// Online sources:   Avoid web searches to solve your problems, but if you do
-//                   search, be sure to include Web URLs and description of
-//                   of any information you find.
-//////////////////////////// 80 columns wide //////////////////////////////////
+// Author:             Michael Korelov
+// Email:              korelovmichael@gmail.com
+///////////////////////////////////////////////////////////////////////////////
 
 /**
- * (Write a succinct description of this class here. You should avoid
- * wordiness and redundancy.)
+ * Class that contains the main method.
  *
- * Bugs: (a list of bugs and other problems)
+ * Bugs: N/A
  *
- * @author (your name)
+ * @author Michael Korelov
  */
 public class Main {
     
     /**
-     * (Write a succinct description of this method here.)
+     * Main method.
      *
-     * @param (parameter name) (Describe the first parameter here)
-     * @param (parameter name) (Do the same for each additional parameter)
-     * @return (description of the return value)
+     * @param String[] args: An array of Strings passed from the command line.
      */
     public static void main(String[] args) throws Exception {
     }
