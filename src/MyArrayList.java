0ace1b4212e4cc71abe188fc5b1e3d476a5feb22
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-04-24 13:51:34 -0700

    code style finished

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -21,12 +21,12 @@
  * the number of free slots in the ArrayList.
  * 
  * 1) Write sanity tests for basic methods.
- * 2) Implement insert(idx, elem), contains(elem), delete(idx), add(elem), 
- * get(idx), size() & getCapacity(). 
+ * 2) Implement insert(idx, elem), contains(elem), delete(idx), get(idx), 
+ * size() & getCapacity(). 
  * 3) Write advanced tests w/ edge cases.
  * 4) Write sanity tests for advanced methods.
  * 5) Implement trimToSize(), toArray(), sort(), set(idx, elem), remove(elem), 
- * isEmpty(), indexOf(elem), ensureCapacity(sz), & clear(). 
+ * isEmpty(), indexOf(elem), ensureCapacity(sz), clear(), & add(elem).
  * 6) Implement the constructor given an array.
  * 7) Write advanced tests w/ edge cases.
  *
@@ -36,14 +36,18 @@
  */
 public class MyArrayList {
     private static int DEFAULT = 10;                // default capacity of MyArrayList
+    private String type;                            // datatype stored in MyArrayList
     private int size;                               // number of elements in MyArrayList
     private int capacity;                           // open slots in backend array in MyArrayList
     private Object[] array;                         // backend array in MyArrayList
 
     /**
      * (Write a succinct description of this method here.)
+     * 
+     * @param (parameter name) (Describe the first parameter here)
      */
-    public MyArrayList() {
+    public MyArrayList(String type) {
+        this.type = type;
         this.array = new Object[DEFAULT];
         this.capacity = DEFAULT;
         this.size = 0;
@@ -53,8 +57,10 @@ public class MyArrayList {
      * (Write a succinct description of this method here.)
      * 
      * @param (parameter name) (Describe the first parameter here)
+     * @param (parameter name) (Describe the first parameter here)
      */
-    public MyArrayList(int cap) {
+    public MyArrayList(String type, int cap) {
+        this.type = type;
         this.array = new Object[cap];
         this.capacity = cap;
         this.size = 0;
@@ -67,18 +73,9 @@ public class MyArrayList {
      * @param (parameter name) (Describe the second parameter here)
      * @return (description of the return value)
      */
-    public boolean add(Object elem) {
-        return true;
-    }
-
-    /**
-     * (Write a succinct description of this method here.)
-     * 
-     * @param (parameter name) (Describe the first parameter here)
-     * @param (parameter name) (Describe the second parameter here)
-     * @return (description of the return value)
-     */
     public boolean insert(Object elem, int idx) {
+        // check that type of elem matches
+        // elem.getClass().getSimpleName()
         if (idx < 0 || idx >= this.size) {
             return false;
         }
--- src/MyArrayListTester.java
+++ src/MyArrayListTester.java
@@ -10,6 +10,10 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+// TODO: Things to test
+// - each myarraylist only holds one type
+// - myarraylist works for multiple object types
+
 /**
  * Contains the JUnit Tests.
  *
@@ -24,9 +28,11 @@ public class MyArrayListTester {
      */
     @Test
     public void testDefaultConstructor() {
-        MyArrayList ls = new MyArrayList();
-        assertEquals(ls.size(), 0);
-        assertEquals(ls.getCapacity(), 10);
+        MyArrayList ls = new MyArrayList("String");
+
+        assertEquals(0, ls.size());
+
+        assertEquals(10, ls.getCapacity());
     }
 
     /**
@@ -34,9 +40,11 @@ public class MyArrayListTester {
      */
     @Test
     public void testConstructor() {
-        MyArrayList ls = new MyArrayList(20);
-        assertEquals(ls.size(), 0);
-        assertEquals(ls.getCapacity(), 20);
+        MyArrayList ls = new MyArrayList("Integer", 20);
+
+        assertEquals(0, ls.size());
+
+        assertEquals(20, ls.getCapacity());
     }
 
     /**
@@ -44,24 +52,11 @@ public class MyArrayListTester {
      */
     @Test
     public void testInsert() {
-        MyArrayList ls = new MyArrayList();
+        MyArrayList ls = new MyArrayList("String");
 
-        assertEquals(ls.insert("Cat",0), true);
-        assertEquals(ls.get(0), "Cat");
+        assertEquals(true, ls.insert("Cat",0));
 
-        assertEquals(ls.insert("Dog", 1), true);
-        assertEquals(ls.get(1), "Dog");
-
-        assertEquals(ls.insert("Snake", 0), true);
-        assertEquals(ls.get(0), "Snake");
-        assertEquals(ls.get(1), "Cat");
-        assertEquals(ls.get(2), "Dog");
-
-        assertEquals(ls.insert("Fish", 5), false);
-
-        assertEquals(ls.insert("Pig", 100), false);
-
-        assertEquals(ls.insert("Hamster", -1), false);
+        assertEquals("Cat", ls.get(0));
     }
 
     /**
@@ -69,20 +64,13 @@ public class MyArrayListTester {
      */
     @Test
     public void testContains() {
-        MyArrayList ls = new MyArrayList();
-        ls.insert("Cat",0);
-        ls.insert("Dog", 1);
-        ls.insert("Snake", 0);
-        ls.insert("Fish", 5);
-        ls.insert("Pig", 100);
-        ls.insert("Hamster", -1);
+        MyArrayList ls = new MyArrayList("String");
 
-        assertEquals(ls.contains("Cat"), true);
-        assertEquals(ls.contains("Dog"), true);
-        assertEquals(ls.contains("Snake"), true);
-        assertEquals(ls.contains("Fish"), false);
-        assertEquals(ls.contains("Pig"), false);
-        assertEquals(ls.contains("Hamster"), false);
+        assertEquals(false, ls.contains("Dog"));
+
+        ls.insert("Dog",0);
+
+        assertEquals(true, ls.contains("Dog"));
     }
 
     /**
@@ -90,23 +78,14 @@ public class MyArrayListTester {
      */
     @Test
     public void testDelete() {
-        MyArrayList ls = new MyArrayList();
-        ls.insert("Cat",0);
-        ls.insert("Dog", 1);
-        ls.insert("Snake", 0);
-        ls.insert("Fish", 5);
-        ls.insert("Pig", 100);
-        ls.insert("Hamster", -1);
+        MyArrayList ls = new MyArrayList("String");
 
-        assertEquals(ls.delete(5), null);
-        assertEquals(ls.delete(0), "Snake");
-        assertEquals(ls.delete(0), "Cat");
-        assertEquals(ls.delete(2), null);
-        assertEquals(ls.delete(100), null);
-        assertEquals(ls.delete(0), "Dog");
+        ls.insert("Fish", 0);
 
-        assertEquals(ls.contains("Snake"), false);
-        assertEquals(ls.contains("Cat"), false);
-        assertEquals(ls.contains("Dog"), false);
+        assertEquals(true, ls.contains("Fish"));
+
+        ls.delete(0);
+
+        assertEquals(false, ls.contains("Fish"));
     }
 }
